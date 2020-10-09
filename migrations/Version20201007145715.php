a4237f2a5f1a5a5e63d9abc6632a179d282ce229
Marcin Szczepanik <marcin.szczepanik@netins.it>
2020-10-09 08:45:28 +0200

    fix

--- migrations/Version20201007145715.php
+++ migrations/Version20201007145715.php
@@ -22,8 +22,8 @@ final class Version20201007145715 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE department (id INT UNSIGNED AUTO_INCREMENT NOT NULL, name VARCHAR(10) NOT NULL, salary_percentage_supplement INT NOT NULL, salary_const_supplement INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE employee (id INT UNSIGNED AUTO_INCREMENT NOT NULL, department_id INT DEFAULT NULL, first_name VARCHAR(10) NOT NULL, last_name VARCHAR(10) NOT NULL, supplement_type VARCHAR(10) NOT NULL, base_salary NUMERIC(10, 0) NOT NULL, hired_at DATETIME NOT NULL, INDEX IDX_5D9F75A1AE80F5DF (department_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE department (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(10) NOT NULL, salary_percentage_supplement INT NOT NULL, salary_const_supplement INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, department_id INT DEFAULT NULL, first_name VARCHAR(10) NOT NULL, last_name VARCHAR(10) NOT NULL, supplement_type VARCHAR(10) NOT NULL, base_salary NUMERIC(10, 0) NOT NULL, hired_at DATETIME NOT NULL, INDEX IDX_5D9F75A1AE80F5DF (department_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE employee ADD CONSTRAINT FK_5D9F75A1AE80F5DF FOREIGN KEY (department_id) REFERENCES department (id)');
     }
 
